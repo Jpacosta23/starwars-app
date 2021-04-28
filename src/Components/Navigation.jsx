3d4574fe6acb4e1b0c3331ab8ee501c5524acb61
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-04-28 18:16:15 -0500

    Bootstrap stylling added to the navigation bar. Search bar created with no functionallity yet

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,24 +1,38 @@
 import React, { Link } from "react-router-dom";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 const Navigation = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to={`/characters/1`}>Characters</Link>
-      </li>
-      <li>
-        <Link to="/planets/1">Planets</Link>
-      </li>
-      <li>
-        <Link to="/vehicles/1">Vehicles</Link>
-      </li>
-      <li>
-        <Link to="/starships/1">Starships</Link>
-      </li>
-    </ul>
+    <>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand as={Link} to="/">
+            STARWARS
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to={`/characters/1`}>
+              Characters
+            </Nav.Link>
+            <Nav.Link as={Link} to="/planets/1">
+              Planets
+            </Nav.Link>
+            <Nav.Link as={Link} to="/vehicles/1">
+              Vehicles
+            </Nav.Link>
+            <Nav.Link as={Link} to="/starships/1">
+              Starships
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+      </>
+    </>
   );
 };
 
